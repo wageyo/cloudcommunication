4d21662d80346be5a0477ce5305ba82c98af3db8
wageyo <ilxly01@126.com>
2015-05-19 17:12:16 +0800

    修正~~

--- src/main/java/com/esd/cloudcommunication/controller/VoteController.java
+++ src/main/java/com/esd/cloudcommunication/controller/VoteController.java
@@ -86,7 +86,7 @@ public class VoteController {
 		out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
 		out.println("<Response>");
 		// 如输入 四位字母加上# 则进action 的url路径
-		out.println("	<Get action=\"/checkcode\" finishkey=\"#\" numdigits=\"5\">");
+		out.println("	<Get action=\"/checkcode\" finishkey=\"#\" >");
 		// out.println("   	<Play>inputcode.wav</Play>");
 		out.println("	</Get>");
 		// 否则播放再次输入录音
@@ -110,38 +110,40 @@ public class VoteController {
 		Calling calling = callingService.getByPrimaryKey(callid);
 		log.info(calling.toString());
 		String code = calling.getVerificationCode();
+		log.info("应输入验证码：" + code);
+		log.info("已输入验证码：" + digits);
 		PrintWriter out = response.getWriter();
 		out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
 		out.println("<Response>");
 		// 如果按键 不为四位数字+# 则返回重新输入
-		String regExp = "^[0-9]{4}#$";
-		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher = pattern.matcher(digits);
-		if (!matcher.find()) {
+//		String regExp = "^[0-9]{4}$";
+//		Pattern pattern = Pattern.compile(regExp);
+//		Matcher matcher = pattern.matcher(digits);
+		if (digits == null || "".equals(digits) || digits.length() < 4 || !isNumber(digits.substring(0,4))) {
 			out.println("	<PlayMix type=\"40\">wrongcode.wav;" + code
 					+ "</PlayMix>");
 			out.println("	<Redirect>/inputcode</Redirect>");
 			out.println("</Response>");
 			out.flush();
 			out.close();
-		}else{
-		// 如果输入的按键内容不符合规范, 则返回重新输入
-		String inputCode = digits.substring(0, 4);
-		// 如果验证码输入不正确,返回重新输入
-		if (!inputCode.equals(code)) {
-			out.println("	<PlayMix type=\"40\">wrongcode.wav;" + code
-					+ "</PlayMix>");
-			out.println("	<Redirect>/inputcode</Redirect>");
+		} else {
+			// 如果输入的按键内容不符合规范, 则返回重新输入
+			String inputCode = digits.substring(0, 4);
+			// 如果验证码输入不正确,返回重新输入
+			if (!inputCode.equals(code)) {
+				out.println("	<PlayMix type=\"40\">wrongcode.wav;" + code
+						+ "</PlayMix>");
+				out.println("	<Redirect>/inputcode</Redirect>");
+				out.println("</Response>");
+				out.flush();
+				out.close();
+			}
+			// 验证码正确则进入投票
+			out.println("	<PlayMix type=\"4\">inputprojectno.wav;</PlayMix>");
+			out.println("	<Redirect>/inputprojectno</Redirect>");
 			out.println("</Response>");
 			out.flush();
 			out.close();
-		}
-		// 验证码正确则进入投票
-		out.println("	<PlayMix type=\"4\">inputprojectno.wav;</PlayMix>");
-		out.println("	<Redirect>/inputprojectno</Redirect>");
-		out.println("</Response>");
-		out.flush();
-		out.close();
 		}
 	}
 
@@ -153,7 +155,7 @@ public class VoteController {
 		PrintWriter out = response.getWriter();
 		out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
 		out.println("<Response>");
-		out.println("	<Get action=\"/checkprojectno\" finishkey=\"#\" numdigits=\"6\">");
+		out.println("	<Get action=\"/checkprojectno\" finishkey=\"#\" >");
 		out.println("	</Get>");
 		out.println("	<PlayMix type=\"4\">inputprojectno.wav;</PlayMix>");
 		out.println("	<Redirect>/inputprojectno</Redirect>");
@@ -172,36 +174,36 @@ public class VoteController {
 		PrintWriter out = response.getWriter();
 		out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
 		out.println("<Response>");
-		log.info("inputProjectNo: " + digits);
+		log.info("输入的项目编号: " + digits);
 		// 如果输入的项目编号 不是 五位数字+# 的格式, 则返回重新输入, 待有项目编码列表后进行验证
-		String regExp = "^[0-9]{5}#$";
-		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher = pattern.matcher(digits);
-		if (!matcher.find()) {
+//		String regExp = "^[0-9]{5}[#]$";
+//		Pattern pattern = Pattern.compile(regExp);
+//		Matcher matcher = pattern.matcher(digits);
+		if (digits == null || "".equals(digits) || digits.length() < 5 || !isNumber(digits.substring(0,5))) {
 			out.println("	<PlayMix type=\"4\">wrongprojectno.wav;</PlayMix>");
 			out.println("	<Redirect>/inputprojectno</Redirect>");
 			out.println("</Response>");
 			out.flush();
 			out.close();
-		}else{
-		//截取五位项目编号
-		String projectNo = digits.substring(0,5);
-		// 正确, 则将投票结果插入数据库
-		Calling calling = callingService.getByPrimaryKey(callid);
-		log.info(calling.toString());
-		calling.setProjectno(projectNo);
-		calling.setIsSuccess(Boolean.TRUE);
-		Boolean bl = callingService.update(calling);
-		if (bl) {
-			out.println("	<Play type=\"4\">thanks.wav</Play>");
-			out.println("	<Redirect>/stopservice</Redirect>");
 		} else {
-			out.println("	<PlayMix type=\"4\">inputprojectno.wav;</PlayMix>");
-			out.println("	<Redirect>/inputprojectno</Redirect>");
-		}
-		out.println("</Response>");
-		out.flush();
-		out.close();
+			// 截取五位项目编号
+			String projectNo = digits.substring(0, 5);
+			// 正确, 则将投票结果插入数据库
+			Calling calling = callingService.getByPrimaryKey(callid);
+			log.info(calling.toString());
+			calling.setProjectno(projectNo);
+			calling.setIsSuccess(Boolean.TRUE);
+			Boolean bl = callingService.update(calling);
+			if (bl) {
+				out.println("	<Play type=\"4\">thanks.wav</Play>");
+				out.println("	<Redirect>/stopservice</Redirect>");
+			} else {
+				out.println("	<PlayMix type=\"4\">inputprojectno.wav;</PlayMix>");
+				out.println("	<Redirect>/inputprojectno</Redirect>");
+			}
+			out.println("</Response>");
+			out.flush();
+			out.close();
 		}
 	}
 
@@ -220,9 +222,17 @@ public class VoteController {
 		out.flush();
 		out.close();
 	}
-	
+
 	public static void main(String[] args) {
 		String s = "12345#";
-		System.out.println(s.substring(0,5));
+		System.out.println(s.substring(0, 5));
+//		System.out.println(VoteController.isNumber("123123"));
+	}
+	
+	private boolean isNumber(String digits){
+		String regExp = "^[0-9]{4,5}$";
+		Pattern pattern = Pattern.compile(regExp);
+		Matcher matcher = pattern.matcher(digits);
+		return matcher.find();
 	}
 }
